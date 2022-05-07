a99869cd9bed36185c8e64d9b03599f66e4e83f1
Janderson FFerreira <ffjanderson@gmail.com>
2022-05-07 17:17:34 -0300

    treino do reels #12 - lista

--- reels_lista/lista_de_tarefas.py
+++ reels_lista/lista_de_tarefas.py
@@ -1,6 +1,7 @@
 def main():
     autor = "Janderson FFerreira @janderson.fferreia"
-    print(f"\n*** Treino - Lista de Tarefas do Dia v1.0 *** (por {autor})\n")
+    print("\n*** Treino - Lista de Tarefas do Dia v1.0 ***"
+          f"(por {autor})\n")
 
     tarefas_do_dia = [
         {"tarefa": "Acordar antes do sol nascer", "feito": True},
@@ -8,13 +9,15 @@ def main():
         {"tarefa": "Organizar Mentoria Dia #2", "feito": True},
         {"tarefa": "Postar Treino no Instagram", "feito": True},
         {"tarefa": "Pagar Contas (Todas em dia)!", "feito": True},
-        {"tarefa": "Assistir um filme", "feito": False},
+        {"tarefa": "Assistir um filme com esposa", "feito": False},
     ]
 
-    print(f"Todas as tarefas: {tarefas_do_dia}")
+    print(f"\n\n Todas as tarefas: {tarefas_do_dia}")
 
-    tarefas_por_fazer = [tarefa for tarefa in tarefas_do_dia if tarefa["feito"]]
+    tarefas_por_fazer = [tarefa["tarefa"] for tarefa in tarefas_do_dia 
+                                if not tarefa["feito"]]
 
-    print(f"Tarefas a fazer: {tarefas_por_fazer}")
+    tarefas_por_fazer_texto = "\n".join(tarefas_por_fazer)
+    print(f"\n\nTarefas a fazer:\n{tarefas_por_fazer_texto}")
 
 main()
